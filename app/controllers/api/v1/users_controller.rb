ee8ad8cbaf83c7a7647654f8f9008c3e6f958667
Alexandre Calaça <alexcalaca@gmail.com>
2023-07-27 02:37:07 -0300

    show search data results

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,14 @@ require 'json'
 
 class Api::V1::UsersController < ApplicationController
   def index
-    @users = ApiService.fetch_users
+    users = ApiService.fetch_users
+
+    if params[:query].present?
+      search_service = SearchService.new(users)
+      @users = search_service.search_by_name(params[:query])
+    else
+      @users = users
+    end    
 
     @users = @users.paginate(page: params[:page], per_page: 10)
   end
